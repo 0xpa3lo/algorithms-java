2644025eb2428f3e12faa7cd8e063f27eb0eab56
Paolo <zivic.paolo@gmail.com>
2024-05-15 20:09:44 +0300

    function changest removelast()

--- dsa/minMaxStack.java
+++ dsa/minMaxStack.java
@@ -6,10 +6,11 @@ class minMaxStack {
         private List<Integer> stack = new ArrayList<Integer>();
         private List<HashMap<String, Integer>> minMaxStack = new ArrayList<>();
 
-        // O(1)
+        // O(1) time complexity and O(1) space complexity
         public int peek(){
             return stack.get(stack.size() - 1);
         }
+        // O(1) time complexity and O(1) space complexity
     public void push(int number){
         HashMap<String, Integer> curr_minMax = new HashMap<>();
         curr_minMax.put("min", number);
@@ -24,13 +25,16 @@ class minMaxStack {
         minMaxStack.add(curr_minMax);
         stack.add(number);
     }
+    // O(1) time complexity and O(1) space complexity
     public int pop(){
-            minMaxStack.removeLast();
-            return stack.removeLast();
+            minMaxStack.remove(minMaxStack.size() - 1);
+            return stack.remove(stack.size() - 1);
     }
+    // O(1) time complexity and O(1) space complexity
     public int getMin(){
         return minMaxStack.get(minMaxStack.size() - 1).get("min");
     }
+    // O(1) time complexity and O(1) space complexity
     public int getMax(){
         return minMaxStack.get(minMaxStack.size() - 1).get("max");
     }
@@ -40,9 +44,10 @@ class minMaxStack {
       var stack = new minMaxStack();
       stack.push(1);
       stack.push(2);
-      stack.push(3);
-      stack.push(4);
       System.out.println(stack.peek());
+      stack.push(3);
+      System.out.println(stack.pop());
+      stack.push(4);
       System.out.println(stack.getMin());
       System.out.println(stack.getMax());
 
