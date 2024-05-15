b48a9574f114cdc4ea79fa8618ae5f0d94328d8d
paolo.zivic <zivic.paolo@gmail.com>
2024-05-15 19:51:41 +0300

    minMaxStack created

--- minMaxStack.java
+++ minMaxStack.java
@@ -1,21 +1,51 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
 
 class minMaxStack {
-    private List<Integer> stack = new ArrayList<Integer>();
+        private List<Integer> stack = new ArrayList<Integer>();
+        private List<HashMap<String, Integer>> minMaxStack = new ArrayList<>();
 
-    public int peak(){
-      return stack.get(stack.size() - 1);
+        // O(1)
+        public int peek(){
+            return stack.get(stack.size() - 1);
+        }
+    public void push(int number){
+        HashMap<String, Integer> curr_minMax = new HashMap<>();
+        curr_minMax.put("min", number);
+        curr_minMax.put("max", number);
+        if (!stack.isEmpty()) {
+            HashMap<String, Integer> lastMinMax = minMaxStack.get(minMaxStack.size() - 1);
+            int newMin = Math.min(number, lastMinMax.get("min"));
+            int newMax = Math.max(number, lastMinMax.get("max"));
+            curr_minMax.put("min", newMin);
+            curr_minMax.put("max", newMax);
+        }
+        minMaxStack.add(curr_minMax);
+        stack.add(number);
     }
-    public int push(int x){
-      stack.add(x);
-      return x;
+    public int pop(){
+            minMaxStack.removeLast();
+            return stack.removeLast();
     }
+    public int getMin(){
+        return minMaxStack.get(minMaxStack.size() - 1).get("min");
+    }
+    public int getMax(){
+        return minMaxStack.get(minMaxStack.size() - 1).get("max");
+    }
+
+
     public static void main(String[] args) {
-      var program = new minMaxStack();
-      program.push(1);
-      var a = program.peak();
-      System.out.println(a);
+      var stack = new minMaxStack();
+      stack.push(1);
+      stack.push(2);
+      stack.push(3);
+      stack.push(4);
+     // System.out.println(stack.peek());
+      System.out.println(stack.getMin());
+      System.out.println(stack.getMax());
+
   }
 
 }
